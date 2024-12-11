de91035d77478c1efafd2ae5802c3e2c39c7dfbd
DanCabAltF4 <dcabezab18@gmail.com>
2024-12-11 19:09:34 +0100

    mensaje

---
+++ src/main/java/com/mycompany/ejeplo1/Ejeplo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejeplo1;
+
+/**
+ *
+ * @author DAM116
+ */
+public class Ejeplo1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Funciona");
+        }
+    }
+}
